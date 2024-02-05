8526b1848bdf941eac5cb27277401e999a837332
husains24 <husainsr24@gmail.com>
2024-02-05 19:18:58 +0530

    added two way binding

--- src/components/TicTacToe/TickTacHeader/Player/GameBoard.jsx
+++ src/components/TicTacToe/TickTacHeader/Player/GameBoard.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 const initialGaBoard = [
-  [null, 1, null],
+  [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGaBoard);
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((preGameBoard) => {
+      const updatedBoard = [
+        ...preGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGaBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
